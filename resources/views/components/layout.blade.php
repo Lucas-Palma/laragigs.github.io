d32e776752e888034db1e897801da0be4534958e
Lucas-Palma <palmalucassantiago@gmail.com>
2022-12-02 16:55:53 -0300

    keep old data in the form

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -12,6 +12,7 @@
             crossorigin="anonymous"
             referrerpolicy="no-referrer"
         />
+        <script src="//unpkg.com/alpinejs" defer></script>
         <script src="https://cdn.tailwindcss.com"></script>
         <script>
             tailwind.config = {
@@ -64,4 +65,5 @@
             >
         </footer>
     </body>
+    <x-flash-message />
 </html>
